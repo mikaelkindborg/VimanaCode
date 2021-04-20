b18ae1550b37be222d8d6941e66cf95a4f62f643
Mikael Kindborg <mikael@kindborg.com>
2021-04-20 13:30:22 +0200

    The basic version as with unnecessary reference parameters removed

---
+++ vimana/v1_plain/workbench.php
@@ -0,0 +1,43 @@
+<?php
+
+//
+// File: workbench.php
+// Date: 2021-04-14
+// Author: Mikael Kinborg
+// Email: mikael@kindborg.com
+// Website: kindborg.com
+// License: Creative Commons: Attribution-ShareAlike - CC BY-SA
+//
+// HOW TO USE
+// ==========
+//
+// Open a terminal window and run:
+//
+// php workbench.php
+//
+// PHP needs to be installed on your computer.
+//
+
+require('interpreter.php');
+require('primitives.php');
+
+$code = <<< CODE
+
+(FACT (N) ((N 0 EQ) (1) (N 1 - FACT N *) IFELSE)) DEF
+100000 (20 FACT) DOTIMES
+
+CODE;
+
+f_eval_string($code);
+
+/*
+
+v1_plain with references:
+(FACT (N) ((N 0 EQ) (1) (N 1 - FACT N *) IFELSE)) DEF
+100000 (20 FACT) DOTIMES
+php v1_plain/workbench.php  8.12s user 0.02s system 99% cpu 8.148 total
+
+
+20 FACT PRINTLN
+
+*/
