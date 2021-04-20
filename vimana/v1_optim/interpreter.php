8ec00a7dd27bd326aeba4fd8ffa0d022172fed49
Mikael Kindborg <mikael@kindborg.com>
2021-04-20 13:31:03 +0200

    Version that optimizes on v1_plain

---
+++ vimana/v1_optim/interpreter.php
@@ -0,0 +1,194 @@
+<?php
+
+//
+// File: interpreter.php
+// Date: 2021-04-20
+// Author: Mikael Kinborg
+// Email: mikael@kindborg.com
+// Website: kindborg.com
+// License: Creative Commons: Attribution-ShareAlike - CC BY-SA
+//
+// This file contains an interpeter for an experimental language
+// named VimanaLang. I have created this project for my own
+// purposes, simply because for the fun of it. The inspiration
+// comes from Lisp and Forth. 
+// I am an old Lisp programmer who did computer science research 
+// using MacLisp, MuLisp, Interlisp-D, Smalltalk, and HyperCard 
+// in the 1980s. 
+//
+// I love Lisp, and I miss the simplicity of MuLisp, MacLisp 
+// and systems like Apple Logo. VimanaLang is in the spirit
+// of Lisp in the 1970s and 80s. Uppercase names!
+//
+// The idea is that you can change everything you want yourself.
+// VimanaLang is not for writing applications, it is for 
+// experimenting with programming language design.
+//
+// I use PHP as the implementation language for this version, 
+// just because I can :) Possibly, I will do a C-implementation
+// that can also run on embedded devides.
+//
+// An application I have in mind is retro-style arcade games.
+// I plan to do this using SDL as the graphics engine.
+//
+
+function f_eval_string($code)
+{
+  $list = f_parse($code);
+  $env = [];
+  $stack = [];
+  $prims = f_create_primitives();
+
+  f_eval_list($list, $env, $stack, $prims);
+}
+
+// For debugging.
+function f_println($str)
+{
+  print($str."\n");
+}
+
+function f_printobj($str, $obj)
+{
+  print($str.": ");
+  print_r($obj);
+  print("\n");
+}
+
+// EvalList evaluates a list as program code.
+// The stack is always passed by reference.
+function f_eval_list($list, &$env, &$stack, $prims)
+{
+  // Eval of a non-list just pushes the value onto the stack.
+  // This is a hack to allow the use of symbols in place of lists
+  // in some cases like IFTRUE and IFELSE.
+  if (!is_array($list)):
+    array_push($stack, f_eval_element($list, $env));
+    return;
+  endif;
+  
+  // Evaluate each element in the list.
+  foreach ($list as $element):
+    f_eval($element, $env, $stack, $prims);
+  endforeach;
+}
+
+// Eval modifies the environment and the stack.
+function f_eval($element, &$env, &$stack, $prims)
+{
+  if (is_string($element)):
+    if (strlen($element) === 0):
+      return;
+    endif;
+
+    // If primitive then run it.
+    if (isset($prims[$element])):
+      $prims[$element]($env, $stack, $prims);
+      return;
+    endif;
+
+    // Is it a function?
+    if (isset($env[$element]) && (is_array($env[$element]) && ($env[$element][0] === "FUN"))):
+      // If it is a function, evaluate it.
+      // Functions evaluate their arguments.
+      f_eval_fun($env[$element], $env, $stack, $prims);
+      return;
+    endif;
+  endif;
+
+  // If it is not a primitive or a function, push the literal value.
+  // Variables are not evaluated here. They are evaluated
+  // when calling a function or performing a primitive.
+  array_push($stack, $element);
+}
+
+// Copies the env table to not overwrite shadowed 
+// variables permanently.
+function f_eval_fun($fun, $env, &$stack, $prims)
+{
+  // Get number of parameters.
+  $local_vars = $fun[1];
+  
+  // Stack order is reverse of param order.
+  $local_vars = array_reverse($local_vars);
+  
+  // Bind parameters to values.
+  foreach ($local_vars as $var):
+    $value = array_pop($stack);
+    $value = f_eval_element($value, $env);
+    $env[$var] = $value;
+  endforeach;
+  
+  // Evaluate the function body.
+  f_eval_list($fun[2], $env, $stack, $prims);
+}
+
+function f_eval_element($element, $env)
+{
+  if (is_string($element)):
+    // Lookup value.
+    $obj = $env[$element];
+    if (isset($obj)):
+      // It is a variable, return its value.
+      return $obj;
+    endif;
+  endif;
+  
+  // Otherwise return the element.
+  return $element;
+}
+
+// Pops an element of the stack and evaluates it.
+function array_pop_eval(&$stack, $env)
+{
+  $value = array_pop($stack);
+  return f_eval_element($value, $env);
+}
+
+// Add a native primitive.
+function f_add_primitive(&$prims, $symbol, $fun)
+{
+  $prims[$symbol] = $fun;
+}
+
+// Parse (tokenize) a string and return a list.
+function f_parse($code)
+{
+  $code = str_replace("(", " ( ", $code);
+  $code = str_replace(")", " ) ", $code);
+  $tokens = preg_split("/[\s]+/", $code);
+  // Remove zero-lenth strings.
+  $tokens = array_filter($tokens,
+    function($token) { return strlen($token) > 0; });
+  $list = f_create_list($tokens);
+  return $list;
+}
+
+// Recursively create the list tree structure.
+function f_create_list(&$tokens)
+{
+  $list = [];
+  
+  while (TRUE):
+    if (count($tokens) === 0):
+      return $list;
+    endif;
+    
+    $next = array_shift($tokens);
+    
+    if ($next === ")"):
+      return $list;
+    endif;
+    
+    if ($next === "("):
+      $next = f_create_list($tokens);
+    endif;
+    
+    // Convert string to number.
+    if (is_numeric($next)):
+      $next = $next + 0;
+    endif;
+    
+    array_push($list, $next);
+  endwhile;
+}
---
+++ vimana/v1_optim/primitives.php
@@ -0,0 +1,266 @@
+<?php
+
+//
+// File: primitives.php
+// Date: 2021-04-14
+// Author: Mikael Kinborg
+// Email: mikael@kindborg.com
+// Website: kindborg.com
+// License: Creative Commons: Attribution-ShareAlike - CC BY-SA
+//
+
+// Create primitives.
+function f_create_primitives()
+{
+  $prims = [];
+  
+  // Helper function.
+  $add = function($symbol, $fun) use (&$prims)
+  {
+    f_add_primitive($prims, $symbol, $fun);
+  };
+  
+  $add("EVAL", function(&$env, &$stack, $prims)
+  {
+    $obj = array_pop($stack);
+    f_eval_list($obj, $env, $stack, $prims);
+  });
+  
+  $add("CALL", function(&$env, &$stack, $prims)
+  {
+    $fun = array_pop($stack);
+    f_eval_fun($fun, $env, $stack, $prims);
+  });
+  
+  $add("SET", function(&$env, &$stack, $prims)
+  {
+    // SET is single assignment
+    $name = array_pop($stack);
+    if (isset($env[$name])):
+      echo "ERROR: ATTEMPT TO SET ASSIGNED VARIABLE: ".$name."\n";
+      exit();
+    endif;
+    $value = array_pop_eval($stack, $env);
+    $env[$name] = $value;
+  });
+  
+  $add("DEF", function(&$env, &$stack, $prims)
+  {
+    $fundef = array_pop($stack);
+    $name = $fundef[0];
+    $params = $fundef[1];
+    $body = $fundef[2];
+    $fun = ["FUN", $params, $body];
+    $env[$name] = $fun;
+  });
+  
+  $add("VALUE", function(&$env, &$stack, $prims)
+  {
+    $var = array_pop($stack);
+    if (is_array($var)):
+      $var = $var[0];
+    endif;
+    $value = $env[$var];
+    array_push($stack, $value);
+  });
+  
+  $add("DOC", function(&$env, &$stack, $prims)
+  {
+    array_pop($stack);
+  });
+  
+  $add("GET", function(&$env, &$stack, $prims)
+  {
+    $index = array_pop_eval($stack, $env);
+    $list = array_pop_eval($stack, $env);
+    array_push($stack, $list[$index - 1]);
+  });
+  
+  $add("COUNT", function(&$env, &$stack, $prims)
+  {
+    $list = array_pop_eval($stack, $env);
+    array_push($stack, count($list));
+  });
+  
+  $add("+", function(&$env, &$stack, $prims)
+  {
+    $b = array_pop_eval($stack, $env);
+    $a = array_pop_eval($stack, $env);
+    $res = $a + $b;
+    array_push($stack, $res);
+  });
+  
+  $add("-", function(&$env, &$stack, $prims)
+  {
+    $b = array_pop_eval($stack, $env);
+    $a = array_pop_eval($stack, $env);
+    $res = $a - $b;
+    array_push($stack, $res);
+  });
+  
+  $add("*", function(&$env, &$stack, $prims)
+  {
+    $b = array_pop_eval($stack, $env);
+    $a = array_pop_eval($stack, $env);
+    $res = $a * $b;
+    array_push($stack, $res);
+  });
+  
+  $add("/", function(&$env, &$stack, $prims)
+  {
+    $b = array_pop_eval($stack, $env);
+    $a = array_pop_eval($stack, $env);
+    $res = $a / $b;
+    array_push($stack, $res);
+  });
+  
+  $add("T", function(&$env, &$stack, $prims)
+  {
+    array_push($stack, "T");
+  });
+  
+  $add("F", function(&$env, &$stack, $prims)
+  {
+    array_push($stack, "F");
+  });
+  
+  $add("NOT", function(&$env, &$stack, $prims)
+  {
+    $bool = array_pop($stack);
+    if ($bool === "F"):
+      $res = "T";
+    else:
+      $res = "F";
+    endif;
+    array_push($stack, $res);
+  });
+  
+  $add("EQ", function(&$env, &$stack, $prims)
+  {
+    $b = array_pop_eval($stack, $env);
+    $a = array_pop_eval($stack, $env);
+    if ($a === $b):
+      $res = "T";
+    else:
+      $res = "F";
+    endif;
+    array_push($stack, $res);
+  });
+  
+  $add("IFTRUE", function(&$env, &$stack, $prims)
+  {
+    $a = array_pop($stack);
+    $c = array_pop($stack);
+    f_eval_list($c, $env, $stack, $prims);
+    $res = array_pop($stack);
+    if ($res === "T"):
+      f_eval_list($a, $env, $stack, $prims);
+    endif;
+  });
+  
+  $add("IFELSE", function(&$env, &$stack, $prims)
+  {
+    $b = array_pop($stack);
+    $a = array_pop($stack);
+    $c = array_pop($stack);
+    f_eval_list($c, $env, $stack, $prims);
+    $res = array_pop($stack);
+    if ($res === "T"):
+      f_eval_list($a, $env, $stack, $prims);
+    else:
+      f_eval_list($b, $env, $stack, $prims);
+    endif;
+  });
+  
+  // DOTIMES is meant for benchmarking. It pushes the last 
+  // result onto othe stack.
+  $add("DOTIMES", function(&$env, &$stack, $prims)
+  {
+    $body = array_pop($stack);
+    $n = array_pop_eval($stack, $env);
+    $n = $n - 1;
+    for  ($i = 0; $i < $n; $i++):
+      f_eval_list($body, $env, $stack, $prims);
+      array_pop($stack);
+    endfor;
+    // Pushes the result of the last interation.
+    f_eval_list($body, $env, $stack, $prims);
+  });
+
+  $add("FETCH", function(&$env, &$stack, $prims)
+  {
+    $url = array_pop_eval($stack, $env);
+    $text = file_get_contents($url);
+    array_push($stack, $text);
+  });
+  
+  $add("JOIN", function(&$env, &$stack, $prims)
+  {
+    $list = array_pop_eval($stack, $env);
+    $string = implode (" ", $list);
+    array_push($stack, $string);
+  });
+  
+  $add("SPACE", function(&$env, &$stack, $prims)
+  {
+    array_push($stack, " ");
+  });
+  
+  $add("PRINT", function(&$env, &$stack, $prims)
+  {
+    $obj = array_pop_eval($stack, $env);
+    print_r($obj);
+  });
+  
+  $add("PRINTLN", function(&$env, &$stack, $prims)
+  {
+    $obj = array_pop_eval($stack, $env);
+    print_r($obj); 
+    print("\n");
+  });
+  
+  $add("ENV", function(&$env, &$stack, $prims)
+  {
+    array_push($stack, $env);
+  });
+  
+  $add("STACK", function(&$env, &$stack, $prims)
+  {
+    array_push($stack, $stack);
+  });
+  
+  $add("PRINTENV", function(&$env, &$stack, $prims)
+  {
+    print_r("PRINTENV:\n");
+    print_r($env);
+  });
+  
+  $add("PRINTSTACK", function(&$env, &$stack, $prims)
+  {
+    print_r("PRINTSTACK:\n");
+    print_r($stack);
+  });
+  
+  $add("MAKEOBJ", function(&$env, &$stack, $prims)
+  {
+    $obj = new stdClass();
+    array_push($stack, $obj);
+  });
+  
+  $add("SETPROP", function(&$env, &$stack, $prims)
+  {
+    $obj = array_pop_eval($stack, $env);
+    $prop_name = array_pop($stack);
+    $prop_value = array_pop_eval($stack, $env);
+    $obj->$prop_name = $prop_value;
+  });
+  
+  $add("GETPROP", function(&$env, &$stack, $prims)
+  {
+    $obj = array_pop_eval($stack, $env);
+    $prop_name = array_pop($stack);
+    array_push($stack, $obj->$prop_name);
+  });
+  
+  return $prims;
+}
