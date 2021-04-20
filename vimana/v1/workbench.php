ed1b035830e7438e7f0e68975d2404fd40846e80
Mikael Kindborg <mikael@kindborg.com>
2021-04-20 14:07:49 +0200

    Updated workbench.php

--- vimana/v1/workbench.php
+++ vimana/v1/workbench.php
@@ -23,8 +23,21 @@ require('primitives.php');
 
 $code = <<< CODE
 
-(Hello World) JOIN PRINTLN
+(FACT (N) ((N 0 EQ) (1) (N 1 - FACT N *) IFELSE)) DEF
+100000 (20 FACT) DOTIMES
 
 CODE;
 
 f_eval_string($code);
+
+/*
+
+v1 with references:
+(FACT (N) ((N 0 EQ) (1) (N 1 - FACT N *) IFELSE)) DEF
+100000 (20 FACT) DOTIMES
+php v1/workbench.php  8.98s user 0.02s system 99% cpu 9.018 total
+
+
+20 FACT PRINTLN
+
+*/
