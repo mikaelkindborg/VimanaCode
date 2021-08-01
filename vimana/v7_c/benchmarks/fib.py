5f44014329cc7084b320cdf6ce146aaf5082e9f9
Mikael Kindborg <mikael@kindborg.com>
2021-08-01 23:00:36 +0200

    Fionacci benchmark

---
+++ vimana/v7_c/benchmarks/fib.py
@@ -0,0 +1,13 @@
+# python benchmarks/fib.py  0.47s user 0.09s system 87% cpu 0.639 total
+# python benchmarks/fib.py  0.43s user 0.03s system 98% cpu 0.464 total
+
+# python3 benchmarks/fib.py  0.57s user 0.03s system 64% cpu 0.933 total
+# python3 benchmarks/fib.py  0.59s user 0.01s system 97% cpu 0.611 total
+
+def fib(n):
+  if n < 2: return n
+  return fib(n - 1) + fib(n - 2)
+
+for i in range(0, 5):
+  print(fib(28))
+
---
+++ vimana/v7_c/benchmarks/fib.rb
@@ -0,0 +1,14 @@
+# ruby benchmarks/fib.rb  0.28s user 0.08s system 83% cpu 0.432 total
+# ruby benchmarks/fib.rb  0.23s user 0.03s system 96% cpu 0.271 total
+
+def fib(n)
+  if n < 2 then
+    n
+  else
+    fib(n - 1) + fib(n - 2)
+  end
+end
+
+for i in 0...5
+  puts fib(28)
+end
