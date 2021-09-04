04a0b326b68ccc951abf02d0ef62062f4d748cc3
Mikael Kindborg <mikael@kindborg.com>
2021-09-04 23:24:51 +0200

    Sketching next step

--- vimana/v8_c_minimal/primfuns.h
+++ vimana/v8_c_minimal/primfuns.h
@@ -16,4 +16,31 @@ switch (primFun)
     ItemSetNumber(item1, ItemNumber(*item1) + ItemNumber(*item2));
     InterpPush(interp, item1);
     break;
+  // funify
+  // setglobal
+  // getat
+  // setat
+  // length
+  // def
+  // eval
+  // sub
+  // sub1
+  // ifelse
+  // iftrue
+  // iffalse
+  // iszero
+  // >, <, eq
+  // drop, swap, dup, over, 2dup
+
+/*
+(FIB)
+  (DUP 2 < (DUP SUB1 FIB SWAP 2 - FIB +) IFFALSE) DEF
+
+(TIMESDO) 
+  (DUP ISZERO 
+    (DROP DROP) 
+    (SWAP DUP EVAL SWAP SUB1 TIMESDO) 
+  IFELSE) DEFSPECIAL
+*/
+
 }
